ecc67fefbfb4702375b9bf35d6e3dbb60b0aae9b
Syed <muhtasham00@gmail.com>
2024-07-29 14:54:41 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const Add = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const Sub = () => {
+    setCounter((prev) => prev - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={Add}>Add bg 1</button>
+      <p> {counter} </p>
+      <button onClick={Sub}>Subtract by 1</button>
     </div>
   );
 }
