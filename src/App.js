b448e20a9bcc1f1aef4048f7da7dbf9b605e4083
Syed <muhtasham00@gmail.com>
2024-07-30 15:36:54 +0500

    form validation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+
 import Comp from "./Comp";
 
 function App() {
--- src/Comp.js
+++ src/Comp.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import "./index.css";
 
 export default function Comp() {
-  let text;
   // const [counter, setCounter] = useState(0);
   const [value, setValue] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordTouched, setPasswordTouched] = useState("");
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [ShowPaswordOption, setShowPaswordOption] = useState(false);
 
   const emailValidate = (email) => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -20,33 +23,92 @@ export default function Comp() {
   };
 
   const passwordValidation = validatePassword(password);
- 
+
+  const isEmailValid = emailTouched && email.length > 0 && emailValidate(email);
+  const passValidation = validatePassword(password);
+  const isPasswordValid =
+    passwordTouched &&
+    password.length > 0 &&
+    passwordValidation.hasCapital &&
+    passwordValidation.hasNumber &&
+    passwordValidation.hasSymbol;
+
   return (
-    <div className="App">
-      <p>Enter Your Name:</p>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      {value.length > 10 && <p>You add more than 10 characters.</p>}
-      <p>Enter your Email:</p>
-      <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
-      {!emailValidate(email) && email.length > 0 && <p>Please enter a valid email.</p>}
-      <p>Enter your Password:</p>
-      <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-      {console.log(password)}
-      <div>
-        <p>Password must contain:</p>
-        <p>
-          {passwordValidation.hasCapital ? "✅" : "❌"} At least one capital letter
-        </p>
-        <p>
-          {passwordValidation.hasNumber ? "✅" : "❌"} At least one number
-        </p>
-        <p>
-          {passwordValidation.hasSymbol ? "✅" : "❌"} At least one symbol
-        </p>
+    <div className="">
+      <div className="w-[90%] m-auto gap-3 flex flex-col justify-center items-center py-10">
+        <p className="">Enter Your Name:</p>
+        <input
+          className="border border-black"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {value.length > 10 && <p>You add more than 10 characters.</p>}
+        <p>Enter your Email:</p>
+        <input
+          className="border border-black"
+          onFocus={() => setEmailTouched(false)}
+          onBlur={() => setEmailTouched(true)}
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        {emailTouched && email.length === 0 && <p>Email cannot be empty.</p>}
+        {!emailValidate(email) && emailTouched && email.length > 0 && (
+          <p>Please enter a valid email.</p>
+        )}
+        <p>Enter your Password:</p>
+        <input
+          type="password"
+          className="border border-black"
+          value={password}
+          onBlur={() => {
+            setPasswordTouched(true);
+          }}
+          onFocus={() => {
+            setPasswordTouched(false);
+          }}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setShowPaswordOption(true);
+          }}
+        />
+        {passwordTouched && password.length === 0 && (
+          <p>Password can not be Empty</p>
+        )}
+        {ShowPaswordOption && (
+          <div className="ml-4">
+            <p>Password must contain:</p>
+            <p>
+              {passwordValidation.hasCapital ? "✅" : "❌"} At least one capital
+              letter
+            </p>
+            <p>
+              {passwordValidation.hasNumber ? "✅" : "❌"} At least one number
+            </p>
+            <p>
+              {passwordValidation.hasSymbol ? "✅" : "❌"} At least one symbol
+            </p>
+          </div>
+        )}
+
+        {isEmailValid && passValidation && isPasswordValid ? (
+          <button
+            disabled={true}
+            className="border border-black px-6 bg-sky-400"
+          >
+            Submit
+          </button>
+        ) : (
+          <button
+            disabled={false}
+            className="border border-black px-6 bg-sky-400"
+          >
+            Submit
+          </button>
+        )}
       </div>
     </div>
   );
