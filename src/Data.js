969aea3cd1179cf6ce200c078b0ea96d78aff437
Syed <muhtasham00@gmail.com>
2024-08-01 16:27:35 +0500

    states & Props

--- src/Data.js
+++ src/Data.js
@@ -1,26 +1,19 @@
 import React, { useState } from "react";
-import Tab1 from "./Tab1";
-import Tab2 from "./Tab2";
+// import Tab1 from "./Tab1";
+// import Tab2 from "./Tab2";
+import Tabs from "./Tabs";
 
 const Data = () => {
   const [name, setName] = useState([]);
-  const [toggle, setToggle] = useState("tab1");
 
   console.log(name);
 
   return (
     <>
-      <div className="flex px-40 py-6 w-[90%] flex-col ">
-        <div className="flex items-center gap-4">
-          <div onClick={() => setToggle("tab1")}>Tab1</div>
-          <div onClick={() => setToggle("tab2")}>Tab2</div>
-        </div>{" "}
-        <br />
         <div>
-          {toggle === "tab1" && <Tab1 name={name} setName={setName} />}
-          {toggle === "tab2" && <Tab2 name={name} />}
+            <Tabs name={name} setName={setName} />
         </div>
-      </div>
+      
     </>
   );
 };
